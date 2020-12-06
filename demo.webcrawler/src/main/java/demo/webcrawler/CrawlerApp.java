3ad5b7226012d52b09ffdc321e53d8652eac2b1b
adeotubusen <adeotubusen@gmail.com>
2020-12-06 17:49:56 +0000

    Added  -CrawlerApp  -CrawlerAppTest

---
+++ demo.webcrawler/src/main/java/demo/webcrawler/CrawlerApp.java
@@ -0,0 +1,13 @@
+package demo.webcrawler;
+
+public class CrawlerApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void crawl() {
+
+	}
+}
---
+++ demo.webcrawler/src/test/java/demo/webcrawler/CrawlerAppTest.java
@@ -0,0 +1,14 @@
+package demo.webcrawler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CrawlerAppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
